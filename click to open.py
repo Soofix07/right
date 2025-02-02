4593ed59427bf0d43cf6c0082df9e210215f0fd1
Soofix07 <soufiane.bekkaoui-etu@etu.univh2c.ma>
2025-02-02 18:15:46 +0100

    inital code

---
+++ click to open.py
@@ -0,0 +1,14 @@
+import win32com.client
+
+# Create an instance of CANoe using its ProgID
+canoe = win32com.client.Dispatch("CANoe.Application.1")
+
+# You can now interact with CANoe. For example:
+# Start CANoe
+canoe.Start()
+
+# Perform other operations, such as opening a configuration file
+# canoe.Configuration.Load(r"C:\path\to\your\configuration.cfg")
+
+# Stop CANoe
+canoe.Stop()
